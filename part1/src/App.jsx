1cc4a2c9b452d7b9f8baba5dba37a07101fea8c3
mersikka <mio@argillander.me>
2025-08-06 16:48:28 +0300

    Complete exercise 2

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -4,19 +4,21 @@ const Header = (props) => {
   )
 }
 
+const Part = (props) => {
+  return (
+    <p>
+      {props.p} {props.e}
+    </p>
+  )
+}
+
 const Content = (props) => {
   return (
-    <>
-      <p>
-        {props.p1} {props.e1}
-      </p>
-      <p>
-        {props.p2} {props.e2}
-      </p>
-      <p>
-        {props.p3} {props.e3}
-      </p>
-    </>
+    <div>
+      <Part p={props.p1} e={props.e1} />
+      <Part p={props.p2} e={props.e2} />
+      <Part p={props.p3} e={props.e3} />
+    </div>
   )
 }
 
